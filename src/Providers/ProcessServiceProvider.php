ccea2ae877e6d9d5a0b48beef1fe4de5e34d6c7b
Ilyas <ilyas@ody.dev>
2025-04-01 20:00:42 +0000

    Merge pull request #25 from ody-dev/develop

--- src/Providers/ProcessServiceProvider.php
+++ src/Providers/ProcessServiceProvider.php
@@ -14,40 +14,38 @@ class ProcessServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (!$this->isRunningInConsole()) {
-            ProcessManager::init([
-                'max_processes' => 256,
-                'enable_coroutine' => true,
-            ]);
+        ProcessManager::init([
+            'max_processes' => 256,
+            'enable_coroutine' => true,
+        ]);
 
-            // Start standard processes
-            $standardProcesses = config('process.processes', []);
-            foreach ($standardProcesses as $process => $args) {
-                if (is_numeric($process)) {
-                    Process::execute($args, []);
-                } else {
-                    Process::execute($process, $args);
-                }
+        // Start standard processes
+        $standardProcesses = config('process.processes', []);
+        foreach ($standardProcesses as $process => $args) {
+            if (is_numeric($process)) {
+                Process::execute($args, []);
+            } else {
+                Process::execute($process, $args);
             }
+        }
 
-            // Start TCP processes
-            $tcpProcesses = config('process.tcp_processes', []);
-            foreach ($tcpProcesses as $process => $args) {
-                if (is_numeric($process)) {
-                    ProcessManager::executeTcp($args);
-                } else {
-                    ProcessManager::executeTcp($process, $args);
-                }
+        // Start TCP processes
+        $tcpProcesses = config('process.tcp_processes', []);
+        foreach ($tcpProcesses as $process => $args) {
+            if (is_numeric($process)) {
+                ProcessManager::executeTcp($args);
+            } else {
+                ProcessManager::executeTcp($process, $args);
             }
+        }
 
-            // Start Unix socket processes
-            $unixProcesses = config('process.unix_processes', []);
-            foreach ($unixProcesses as $process => $args) {
-                if (is_numeric($process)) {
-                    ProcessManager::executeUnix($args);
-                } else {
-                    ProcessManager::executeUnix($process, $args);
-                }
+        // Start Unix socket processes
+        $unixProcesses = config('process.unix_processes', []);
+        foreach ($unixProcesses as $process => $args) {
+            if (is_numeric($process)) {
+                ProcessManager::executeUnix($args);
+            } else {
+                ProcessManager::executeUnix($process, $args);
             }
         }
     }
